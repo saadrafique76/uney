d2b924f54641d150171f81c5c380799e24869469
saadrafique76 <92212274+saadrafique76@users.noreply.github.com>
2025-06-10 21:43:50 +0400

    Update server.js

--- app/webserver/server.js
+++ app/webserver/server.js
@@ -1,1 +1,51 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
+const PORT = process.env.PORT || 80;
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+  const extname = path.extname(filePath);
+  let contentType = 'text/html';
+
+  switch (extname) {
+    case '.js':
+      contentType = 'text/javascript';
+      break;
+    case '.css':
+      contentType = 'text/css';
+      break;
+    case '.json':
+      contentType = 'application/json';
+      break;
+    case '.png':
+      contentType = 'image/png';
+      break;
+    case '.jpg':
+      contentType = 'image/jpg';
+      break;
+    case '.ico':
+      contentType = 'image/x-icon';
+      break;
+  }
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code == 'ENOENT') {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end('<h1>404 Not Found</h1>');
+      } else {
+        res.writeHead(500);
+        res.end('Server Error: ' + err.code);
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content, 'utf8');
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Web server running on port ${PORT}`);
+});
