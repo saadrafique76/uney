96ac1d28f85b1d40bde8e490ca66769d2fb1b820
saadrafique76 <92212274+saadrafique76@users.noreply.github.com>
2025-06-10 21:54:16 +0400

    Update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,1 +1,14 @@
+terraform {
+  required_version = ">= 1.0.0"
 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0" # Use a compatible AWS provider version
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0" # Required if you add Kubernetes provider interaction
+    }
+  }
+}
